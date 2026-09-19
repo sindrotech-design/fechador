e7e9364254d812c712136de0cafef7fe68b6c258
SindroTech <sindrotech@gmail.com>
2026-09-19 03:21:43 -0300

    fix: add require for qrcode-terminal in ESM

--- server/services/whatsapp.ts
+++ server/services/whatsapp.ts
@@ -1,3 +1,6 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 import { proto, WASocket } from '@whiskeysockets/baileys';
 import { useMultiFileAuthState } from '@whiskeysockets/baileys';
 import pino from 'pino';
