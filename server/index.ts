d010fec4203c842fa3b68c34ff06cd0d58b62f4f
SindroTech <sindrotech@gmail.com>
2026-09-21 16:51:37 -0300

    fix: use require for glob import

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { execSync } from 'child_process';
 import fs from 'fs';
-import glob from 'glob';
+const glob = require('glob');
 
 import whatsappRoutes from './routes/whatsapp.js';
 import { whatsappService } from './services/whatsapp.js';
