cd24e871c39efa2d000b21b6fca8d9bcc9e0fae4
SindroTech <sindrotech@gmail.com>
2026-09-22 04:49:36 -0300

    chore: update .env.example for local dev, remove docker/test files

--- test-server.mjs
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-const server = http.createServer((req, res) => {
-  res.end('Hello from background');
-});
-server.listen(43135, '127.0.0.1', () => {
-  console.log('BG Server listening on 127.0.0.1:43135');
-});
-server.on('error', console.error);
