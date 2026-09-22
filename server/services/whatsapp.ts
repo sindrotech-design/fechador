e72d13e0b22a3c325888f08871160ed9c5c3e302
SindroTech <sindrotech@gmail.com>
2026-09-22 14:32:11 -0300

    fix: remove problematic Chrome flags for Windows

--- server/services/whatsapp.ts
+++ server/services/whatsapp.ts
@@ -94,8 +94,6 @@ export class WhatsAppService extends EventEmitter {
             '--disable-dev-shm-usage',
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
-            '--no-zygote',
-            '--single-process',
             '--disable-gpu'
           ],
         },
